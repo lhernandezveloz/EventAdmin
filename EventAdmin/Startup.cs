9083827b99dcd9d07e03805594ce0c610db8df34
leonalhernandezveloz <leonalhernandezveloz@LEONALHERNAA4A0>
2020-04-08 12:21:22 -0500

    Add project files.

---
+++ EventAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventAdmin.Startup))]
+namespace EventAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
