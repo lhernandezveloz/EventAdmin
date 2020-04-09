607c971d477db64e24ba25569252db247dd11bcf
Leonal Hernandez Veloz <lhernandezveloz@gmail.com>
2020-04-09 18:56:22 -0500

    Adding Add Concert Screen

---
+++ EventAdmin/Controllers/ConcertsController.cs
@@ -0,0 +1,27 @@
+﻿using EventAdmin.Models;
+using EventAdmin.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace EventAdmin.Controllers
+{
+    public class ConcertsController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ConcertsController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new ConcertFormViewModel
+            {
+                Genres = _dbContext.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ EventAdmin/ViewModels/ConcertFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using EventAdmin.Models;
+using System.Collections.Generic;
+
+namespace EventAdmin.ViewModels
+{
+    public class ConcertFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
